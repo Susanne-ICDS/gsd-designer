118923da7141df7ac56661730983c3dc6d60705f
Susanne-ICDS <susanne.blotwijk@vub.be>
2021-05-05 11:53:11 +0200

    Removed files people don't need to see

--- memprof.bat
+++
@@ -1,3 +0,0 @@
-python -m memory_profiler test_and_timer.py
-mprof run test_and_timer.py
-mprof plot
--- push.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-
-if not exist Version.txt >Version.txt echo 0
-for /f %%x in (Version.txt) do (
-set /a ver=%%x+1
-)
-echo %ver% > Version.txt 
-echo Dit is versie %ver%
