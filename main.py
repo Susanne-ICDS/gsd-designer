92006822a69a1b1b6170908433e959ba05e52d04
Susanne-ICDS <susanne.blotwijk@vub.be>
2021-05-19 12:24:08 +0200

    Added .yaml and added firebase script to html

--- main.py
+++ main.py
@@ -12,11 +12,15 @@ from tabs import navbar, tab_basic_design, tab_interim_analyses, tab_error_spend
 
 from layout_instructions import spacing_variables as spacing
 
+app = server
 
 BISI_LOGO = '/assets/Logos.svg'
 version = '1.1.' + str(int(open("version.txt", "r").read()))
+local = False
+memory_limit = 1 - 0.25  # in GigaByte
+callbacks.create_evaluation(local, memory_limit)
 
-dash_app.layout = html.Div([navbar.bar, dbc.Container([
+dash_app.layout = html.Div(children=[navbar.bar, dbc.Container([
 
     dcc.Location(id='url', pathname='/basic-design', refresh=False),
 
@@ -24,6 +28,7 @@ dash_app.layout = html.Div([navbar.bar, dbc.Container([
     html.Div(id='interim-analyses', hidden=True, children=tab_interim_analyses.layout),
     html.Div(id='error-spending', hidden=True, children=tab_error_spending.layout),
     html.Div(id='simulation', hidden=True, children=tab_simulation.layout),
+
 
     html.Br(),
     html.Br(),
@@ -81,10 +86,6 @@ dash_app.layout = html.Div([navbar.bar, dbc.Container([
     # This is a list of two json serialized pandas dataframes 1) the estimates and 2) their standard errors.
 
 ], fluid=True)])
-
-local = False
-memory_limit = 1 - 0.25  # in GigaByte
-callbacks.create_evaluation(local, memory_limit)
 
 if __name__ == '__main__':
     dash_app.run_server(debug=True)
